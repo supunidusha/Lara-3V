e0e1c0aaaa742bc923929cdfa28640fa40642623
supunidusha <supunidusha3@gmail.com>
2026-02-19 00:02:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
-PREFIX: process.env.PREFIX || ".",
+SESSION_ID: process.env.SESSION_ID || "uMpiWBhY#1bVuMQHctnFOQVCd_DnUUB3AIurfl1jjJOgFQxq4_YI", // Add your session id
+PREFIX: process.env.PREFIX || "_",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
 DEV: process.env.DEV || "94779062397",
